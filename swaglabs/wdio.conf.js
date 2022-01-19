5deaf0865fecdfa709a0943b114953dccf4e20b9
JuelHossain <juelhossainnn@gmail.com>
2022-01-19 21:11:59 +0600

    chromedriver

--- swaglabs/wdio.conf.js
+++ swaglabs/wdio.conf.js
@@ -1,5 +1,4 @@
 
-import allure from "allure-commandline";
 exports.config = {
   //
 
@@ -150,10 +149,10 @@ exports.config = {
   key: process.env.BROWSERSTACK_ACCESS_KEY,
   services: [
     [
-      "browserstack",
-      {
-        browserstackLocal: false,
-      },
+      "chromedriver",
+      // {
+      //   browserstackLocal: false,
+      // },
     ],
   ],
   
@@ -322,24 +321,24 @@ exports.config = {
    * @param {Array.<Object>} capabilities list of capabilities details
    * @param {<Object>} results object containing test results
    */
-  onComplete: function () {
-    const reportError = new Error("Could not generate Allure report");
-    const generation = allure(["generate", "allure-results", "--clean"]);
-    return new Promise((resolve, reject) => {
-      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+  // onComplete: function () {
+  //   const reportError = new Error("Could not generate Allure report");
+  //   const generation = allure(["generate", "allure-results", "--clean"]);
+  //   return new Promise((resolve, reject) => {
+  //     const generationTimeout = setTimeout(() => reject(reportError), 5000);
 
-      generation.on("exit", function (exitCode) {
-        clearTimeout(generationTimeout);
+  //     generation.on("exit", function (exitCode) {
+  //       clearTimeout(generationTimeout);
 
-        if (exitCode !== 0) {
-          return reject(reportError);
-        }
+  //       if (exitCode !== 0) {
+  //         return reject(reportError);
+  //       }
 
-        console.log("Allure report successfully generated");
-        resolve();
-      });
-    });
-  },
+  //       console.log("Allure report successfully generated");
+  //       resolve();
+  //     });
+  //   });
+  // },
   /**
    * Gets executed when a refresh happens.
    * @param {String} oldSessionId session ID of the old session
